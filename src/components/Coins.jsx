c59e5d8d0357e5057c0a9f84b73a48c4392b46d7
Bishal Tamang <tbishal078@gmail.com>
2023-05-29 13:23:36 +0545

    first commit

---
+++ src/components/Coins.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import CoinItem from './CoinItem'
+import Coin from '../Pages/Coin'
+
+import { Link } from 'react-router-dom'
+
+import './Coins.css'
+
+const Coins = (props) => {
+  return (
+    <div className='container'>
+       <div>
+            <div className="heading">
+                <p>#</p>
+                <p className='coin-name'>Coin</p>
+                <p>Price</p>
+                <p>24h</p>
+                <p className='hide-mobile'>Volume</p>
+                <p className='hide-mobile'>Mkt Cap</p>
+            </div>
+
+            {props.coins.map(coin =>{
+                return(
+                  <Link to={`/coin/${coin.id}`} element={<Coin />} key={coin.id}>
+                  
+                    <CoinItem coins={coin} />
+                  </Link>
+                )
+            })}
+       </div>
+    </div>
+  )
+}
+
+export default Coins
